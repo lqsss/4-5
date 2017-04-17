fee68321ebd6be875ba150f404c8a9e0acf141c2
lqs_pc\liqiushi <513447887@qq.com>
2017-04-17 23:35:44 +0800

    导航start！next: code style！

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <v-header>
-
     </v-header>
-    <div class="tab">
+    <div class="tab">   <!--导航-->
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评价</div>
+      <div class="tab-item">商家</div>
       I am tab!
     </div>
     <div class="content">
