324ea72a83a898b424364bc3a1a301ba54741ded
lqs_pc\liqiushi <513447887@qq.com>
2017-04-21 00:22:17 +0800

    (5个=>)

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,18 +17,28 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div class="support-count" v-if="seller.supports">
+      <div class="support-count" v-if="seller.supports" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper"  @click="showDetail">
       <span class="bulletin-icon"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
-      <img :src="seller.avatar" width="100%" height="100%">
+      <img :src="seller.avatar" width="100%" height="100%">    <!-- 撑满-->
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 
@@ -167,8 +177,7 @@
         background-size:22px,12px
         background-repeat no-repeat
       .bulletin-text
-        //display:inline-block
-        //vertical-align: top
+
         margin-left:4px
         line-height:28px
         font-weight 200
@@ -189,4 +198,50 @@
       z-index: -1
       filter: blur(10px)
 
+    .detail
+      position:fixed   //相对浏览器
+      z-index :1
+      top:0
+      left:0
+      width: 100%
+      height:100%
+      overflow:auto
+      background:rgba(7,17,27,0.8)
+      .detail-wrapper
+        min-height:100%   //一个块元素的最小高度 如果再写 块元素就不会再自动变小 而保持此最小高度
+        width: 100%
+      .clearfix
+        display:inline-block
+        &:after
+          display:block
+          content:'.'
+          height:0
+          line-height:0
+          clear:both
+          visibility:hidden
+        .detail-main
+            margin-top:64px
+            padding-bottom:64px
+          .name
+            line-height: 16px
+            text-align:center
+            font-size:16px
+            font-weight:700
+      .detail-close
+        position:relative
+        width: 32px
+        height: 32px
+        //margin: 0px auto 64px auto
+        margin: -64px auto 0px auto
+        //clear:both
+        font-size:32px
+
+/*
+    class="a b"
+
+    stylus
+    .a
+      //a的style内容;
+    .b
+*/
 </style>
