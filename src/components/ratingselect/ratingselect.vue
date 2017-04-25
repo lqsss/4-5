b4aee305fecf81806dd0eb23e2d41ade0b86a2dc
admin <513447887@qq.com>
2017-04-25 17:36:13 +0800

    no message

---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ratingselect">
+    <div class="rating-type">
+      <span class="block positive"></span>
+      <span class="block positive"></span>
+      <span class="block positive"></span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default{
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
